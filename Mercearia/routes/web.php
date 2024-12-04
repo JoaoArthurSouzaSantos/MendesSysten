4d03f20f53b06b066f81cfeec371b87e2d7878cf
JoaoArthurSouzaSantos <joaoarthursouzasantos@gmail.com>
2024-12-04 08:22:34 -0300

    importa e publica as configurações do laravel ui

--- Mercearia/routes/web.php
+++ Mercearia/routes/web.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        
+    });
+});
+
+Route::prefix('cliente')->group(function () {
+    Route::get('/users', function () {
+       
+    });
+});
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
